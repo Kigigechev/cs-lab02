0f1d56f145b0eceb617aae326427273745392211
Alice (Kigigechev) <alice@example.com>
2021-03-01 15:57:52 +0300

    git: сумма и разность

--- main.cpp
+++ main.cpp
@@ -7,5 +7,7 @@ int main()
     cout << "Enter A and B";
     int a,b;
     cin >> a >> b;
-return 0;
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n';
+    return 0;
 }
